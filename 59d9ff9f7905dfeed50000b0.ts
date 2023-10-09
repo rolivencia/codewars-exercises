aa1566867acd4df56e9a847428a87996a049546e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-10-09 08:14:36 -0300

    Add solution for Alphabet Symmetry - 59d9ff9f7905dfeed50000b0

---
+++ 59d9ff9f7905dfeed50000b0.ts
@@ -0,0 +1,20 @@
+// Alphabet symmetry (7 kyu) - https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0
+
+export function letterOrder(word: string, index: number){
+    return (word.charCodeAt(index) - 96)
+}
+
+export function processWord(word: string): number {
+    const wordInLowerCase = word.toLowerCase()
+    const indexes = []
+    for(let index = 0; index < word.length; index++){
+        if(letterOrder(wordInLowerCase, index) === index + 1){
+            indexes.push(letterOrder(wordInLowerCase, index))
+        }
+    }
+    return indexes.map(x => 1).reduce((x,y) => x+y, 0)
+}
+
+export function solve(words: string[]): number[] {
+    return words.map(word => processWord(word))
+}
