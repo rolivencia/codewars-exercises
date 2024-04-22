c9dfefcd164c15d6f61922a8fd27ce09ab51d684
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-04-22 07:25:39 -0300

    Add solution for the 58daa7617332e59593000006 problem in CodeWars: 'Most digits' (7 kyu)

---
+++ 58daa7617332e59593000006.ts
@@ -0,0 +1,21 @@
+// Solution for the 58daa7617332e59593000006 problem in CodeWars: 'Most digits' (7 kyu)
+
+
+export class Kata {
+
+    static findLongest(array:number[]):number {
+        let result = 0;
+
+        for(let i = result; i < array.length; i++){
+            if(Kata.getNumberDigits(array[i]) > Kata.getNumberDigits(result)){
+                result = array[i]
+            }
+        }
+
+        return result
+    }
+
+    static getNumberDigits(n:number):number {
+        return n.toString().split('').length;
+    }
+}
