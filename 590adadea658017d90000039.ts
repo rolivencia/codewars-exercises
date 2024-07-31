1e88b8911021ae39248e6c2db518fe9399183e28
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-07-31 13:41:14 -0300

    Solution for the 59e9f404fc3c49ab24000112 problem in CodeWars: Ch411eng3 (7 kyu)

---
+++ 590adadea658017d90000039.ts
@@ -0,0 +1,18 @@
+// Solution for the 590adadea658017d90000039 problem in CodeWars: Fruit Machine (6 kyu)
+
+const scorings = [
+    { item: 'Wild', three: 100, two: 10, bonus: 0 },
+    { item: 'Star', three: 90, two: 9, bonus: 18 },
+    { item: 'Bell', three: 80, two: 8, bonus: 16 },
+    { item: 'Shell', three: 70, two: 7, bonus: 14 },
+    { item: 'Seven', three: 60, two: 6, bonus: 12 },
+    { item: 'Cherry', three: 50, two: 5, bonus: 10 },
+    { item: 'Bar', three: 40, two: 4, bonus: 8 },
+    { item: 'King', three: 30, two: 3, bonus: 6 },
+    { item: 'Queen', three: 20, two: 2, bonus: 4 },
+    { item: 'Jack', three: 10, two: 1, bonus: 2 }
+]
+
+export function fruit(reels: string[][], spins: number[]): number{
+    // Good luck
+}
