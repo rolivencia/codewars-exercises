8fd7b89f6eacbb44368299cc29c19d1bf4194214
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-08-26 10:32:45 -0300

    Add solution for the 57a4d500e298a7952100035d problem at CodeWars: Hex to Decimal (8 kyu)

---
+++ 8-kyu/57a4d500e298a7952100035d.ts
@@ -0,0 +1,5 @@
+// Add solution for the 57a4d500e298a7952100035d problem at CodeWars: Hex to Decimal (8 kyu)
+
+export function hexToDec(hexString:string):number{
+    return parseInt(hexString, 16)
+}
