17865bc5ec81793475e540a49aa111e2c110adec
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-08 14:51:34 -0300

    Solution for the 5a523566b3bfa84c2e00010b problem at CodeWars: Minimize Sum Of Array (Array Series #1) (7 kyu)

---
+++ 7-kyu/5a523566b3bfa84c2e00010b.ts
@@ -0,0 +1,6 @@
+// Solution for the 5a523566b3bfa84c2e00010b problem at CodeWars: Minimize Sum Of Array (Array Series #1) (7 kyu)
+export function minSum(arr: number[]): number {
+    return arr
+        .sort((a, b) => a - b)
+        .reduce((accum, current, index) => accum + current * arr[arr.length - 1 - index], 0) / 2
+}
