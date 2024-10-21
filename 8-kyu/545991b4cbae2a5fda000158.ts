a28a82837fbec47cecc31bed817dc2effccd9588
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-10-21 09:06:20 -0300

    Solution for the 545991b4cbae2a5fda000158 problem at CodeWars: Enumerable Magic - Does My List Include This? (8 kyu)

---
+++ 8-kyu/545991b4cbae2a5fda000158.ts
@@ -0,0 +1,5 @@
+// Solution for the 545991b4cbae2a5fda000158 problem at CodeWars: Enumerable Magic - Does My List Include This? (8 kyu)
+
+function include(arr, item){
+    return arr.includes(item);
+}
