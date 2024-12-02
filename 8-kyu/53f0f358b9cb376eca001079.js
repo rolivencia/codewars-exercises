4c4e0baa4247f830edf9018e0a0a786fcfd28582
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-12-02 01:15:46 -0300

    Solution for the 53f0f358b9cb376eca001079 problem at CodeWars (8 kyu)

---
+++ 8-kyu/53f0f358b9cb376eca001079.js
@@ -0,0 +1,9 @@
+// Solution for the 53f0f358b9cb376eca001079 problem at CodeWars (8 kyu)
+
+var Ball = function(ballType) {
+  if(!ballType){
+    this.ballType ='regular'
+  } else {
+    this.ballType = ballType
+  }
+};
