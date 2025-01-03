5fcf3e34d9f541ee42e19e9e2b86952935d45925
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-01-02 23:54:11 -0300

    [CodeWars] Solution for the 55f73f66d160f1f1db000059 problem at CodeWars: Grasshopper - Combine strings (8kyu)

---
+++ 8-kyu/55f73f66d160f1f1db000059.js
@@ -0,0 +1,6 @@
+// Solution for the 55f73f66d160f1f1db000059 problem at CodeWars: Grasshopper - Combine strings (8kyu)
+
+// Create the combineNames function here
+function combineNames(firstName, lastName){
+    return `${firstName} ${lastName}`
+}
