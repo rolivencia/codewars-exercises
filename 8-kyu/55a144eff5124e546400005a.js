2fd23e53c7ac710c58e56cc4d5213be71e531ece
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-01-16 12:33:04 -0300

    [CodeWars] Solution for the 55a144eff5124e546400005a problem at CodeWars: Classy Classes (8 kyu)

---
+++ 8-kyu/55a144eff5124e546400005a.js
@@ -0,0 +1,7 @@
+// Solution for the 55a144eff5124e546400005a problem at CodeWars: Classy Classes (8 kyu)
+
+class Person {
+    constructor(name, age) {
+        this.info = `${name}s age is ${age}`
+    }
+}
