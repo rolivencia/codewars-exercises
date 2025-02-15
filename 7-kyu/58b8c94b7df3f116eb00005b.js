2ebd2d7229f81d5f281851a574a4e1567db7e7b7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-02-15 03:15:24 -0300

    [CodeWars] Solution for the 58b8c94b7df3f116eb00005b problem at CodeWars: Simple Fun #176: Reverse Letter (7 kyu)

--- 7-kyu/58b8c94b7df3f116eb00005b.js
+++ 7-kyu/58b8c94b7df3f116eb00005b.js
@@ -1,5 +1,5 @@
 // Solution for the 58b8c94b7df3f116eb00005b problem at CodeWars: Simple Fun #176: Reverse Letter (7 kyu)
 
 function reverseLetter(str) {
-    return str.split('').reverse().join('').replace(/[^a-zA-z]/g, '')
+    return str.split('').reverse().join('').replace(/[^a-z]/g, '')
 }
