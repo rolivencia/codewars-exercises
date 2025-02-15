12dd8795d45cc4a887fcac168c7c11bd41c79005
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-02-15 03:09:58 -0300

    [CodeWars] Solution for the 562926c855ca9fdc4800005b problem at CodeWars: Power (8 kyu)

--- 8-kyu/562926c855ca9fdc4800005b.js
+++ 8-kyu/562926c855ca9fdc4800005b.js
@@ -1,6 +1,9 @@
 // Solution for the 562926c855ca9fdc4800005b problem at CodeWars: Power (8 kyu)
 
 function numberToPower(number, power){
+    if(power === 0){
+        return 1
+    }
     if(number === 0){
         return 0
     }
