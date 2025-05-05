a5b5034097408bf9481c9a77680487224f807b00
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-05-05 14:13:04 -0300

    [CodeWars] Solution for the 5ba38ba180824a86850000f7 problem at CodeWars: Simple remove duplicates (7 kyu)

---
+++ 7-kyu/5ba38ba180824a86850000f7.js
@@ -0,0 +1,12 @@
+// Solution for the 5ba38ba180824a86850000f7 problem at CodeWars: Simple remove duplicates (7 kyu)
+
+function solve(arr) {
+    const result = []
+    for(let element of arr.reverse()){
+        if(result.find((e) => element === e)){
+            continue
+        }
+        result.push(element)
+    }
+    return result.reverse()
+}
