69eaf39a480af832682eaa082ba75a9bd2e72e82
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-07-01 23:03:32 -0300

    [CodeWars] Solution for the 582dafb611d576b745000b74 problem at CodeWars: For UFC Fans (Total Beginners): Conor Mcgregor vs George Saint Pierre (8 kyu)

---
+++ 8-kyu/582dafb611d576b745000b74.js
@@ -0,0 +1,17 @@
+// Solution for the 582dafb611d576b745000b74 problem at CodeWars: For UFC Fans (Total Beginners): Conor Mcgregor vs George Saint Pierre (8 kyu)
+
+function quote(fighter) {
+
+    const stringToCheck = fighter.toLowerCase()
+
+    if(stringToCheck === 'george saint pierre'){
+        return "I am not impressed by your performance."
+
+    }
+
+    else {
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+    }
+
+};
