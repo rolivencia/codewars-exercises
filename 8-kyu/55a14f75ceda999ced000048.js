8e67c116a8d429ff304b50ab230879bb3f299ac7
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-07-04 06:32:06 -0300

    [CodeWars] Solution for the 55a14f75ceda999ced000048 problem at CodeWars: Template Strings (8 kyu)

---
+++ 8-kyu/55a14f75ceda999ced000048.js
@@ -0,0 +1,5 @@
+// Solution for the 55a14f75ceda999ced000048 problem at CodeWars: Template Strings (8 kyu)
+
+var templateStrings = function(noun, adjective) {
+    return `${noun} are ${adjective}`
+}
