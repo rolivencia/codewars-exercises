8cc1995ed796655a39ca58a4d70dafb0b0b5054d
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-09-18 14:08:27 -0300

    [CodeWars] Solution for the 55ccdf1512938ce3ac000056 problem at CodeWars: A Strange Trip to the Market (8 kyu)

---
+++ codewars/8-kyu/55ccdf1512938ce3ac000056.js
@@ -0,0 +1,5 @@
+// Solution for the 55ccdf1512938ce3ac000056 problem at CodeWars: A Strange Trip to the Market (8 kyu)
+
+function isLochNessMonster(s) {
+    return s.includes('tree fiddy') || s.includes('three fifty') || s.includes('3.50')
+}
