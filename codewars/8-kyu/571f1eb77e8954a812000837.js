dec35a8917ab91bfac8a9eb67f86b962565d9d93
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-03 12:43:18 -0300

    [CodeWars] Solution for the 571f1eb77e8954a812000837 problem at CodeWars: Training JS #5: Basic data types--Object

---
+++ codewars/8-kyu/571f1eb77e8954a812000837.js
@@ -0,0 +1,6 @@
+// Solution for the 571f1eb77e8954a812000837 problem at CodeWars: Training JS #5: Basic data types--Object
+
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
